ca6f733e364232ef1e41687be7fa2e128794c661
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-09 20:30:50 +0530

    insert value in table

--- src/Sample.java
+++ src/Sample.java
@@ -1,4 +1,4 @@
-import java.sql.Connection;
+/*import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -10,13 +10,27 @@ public class Sample {
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection con= DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/anshika","root","");
-//here sonoo is database name, root is username and password
+
                 Statement stmt=con.createStatement();
                 ResultSet rs=stmt.executeQuery("select * from test1");
                 while(rs.next())
                     System.out.println(rs.getString(1)+"  "+rs.getInt(2));
+                rs.close();
                 con.close();
             }catch(Exception e){ System.out.println(e);}
 
+    }
+}*/
+//Statement interface
+//to insert value in database table.
+import java.sql.*;
+public class Sample{
+    public static void main(String[] args)throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/anshika","root","");
+        Statement stmt=con.createStatement();
+       int i= stmt.executeUpdate("insert into test1 values('akansha',11)");
+        System.out.println(i+"record inserted");
+       con.close();
     }
 }
