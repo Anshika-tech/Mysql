81bc56e388468cf5ab4e2aeb1ff53f52d791ac38
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-10 01:10:48 +0530

    PreparedStatement interface

--- src/Sample.java
+++ src/Sample.java
@@ -33,7 +33,7 @@ public class Sample{
         System.out.println(i+"record inserted");
        con.close();
     }
-}*/
+}
 //delete value from database table
 import java.sql.*;
 public class Sample{
@@ -43,6 +43,24 @@ public class Sample{
         Statement stmt=con.createStatement();
         int i=stmt.executeUpdate("delete from test1 where b1=10");
         System.out.println(i+ " record deleted");
+        con.close();
+    }
+}*/
+//PreparedStatement interface
+//The PreparedStatement interface is a subinterface of Statement. It is used to execute parameterized query.
+import java.sql.*;
+public class Sample{
+    public static void main(String[] args)throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/anshika","root","");
+        String sql="insert into test1 values(?,?)";
+        PreparedStatement stmt= con.prepareStatement(sql);
+        stmt.setString(1,"Priyanshi");
+        stmt.setInt(2,101);
+        stmt.setInt(2,111);
+        stmt.setString(1,"hritik");
+        int i= stmt.executeUpdate();//if update successfully done then i=1.
+        System.out.println(i+ " records inserted");
         con.close();
     }
 }
