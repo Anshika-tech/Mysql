5290f517d62d7f9dfe8fa72a93a3841bef133711
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-11 00:24:39 +0530

    Regex Regular expression

--- src/Regex.java
+++ src/Regex.java
@@ -2,7 +2,7 @@
 //Matcher class
 //It implements the MatchResult interface.It is a regex engine which is used to perform match operations on a character sequence.
 //Three ways to write regex .
-import java.util.regex.*;
+/*import java.util.regex.*;
 public class Regex{
     public static void main(String[] args) {
      //1st way
@@ -14,5 +14,15 @@ public class Regex{
     //3rd way
     boolean b3=Pattern.matches(".s","as");
         System.out.println(b+" "+b2+" "+b3);
+    }
+}*/
+import java.util.regex.*;
+public class Regex{
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches(".s","as"));//true (2nd char is s)
+        System.out.println(Pattern.matches(".s","mk"));//false (2nd char is not s)
+        System.out.println(Pattern.matches(".s","mst"));//false (has more than 2 char)
+        System.out.println(Pattern.matches(".s","amms"));//false (has more than 2 char)
+        System.out.println(Pattern.matches("..s","mas"));//true (3rd char is s)
     }
 }
