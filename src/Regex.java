5e3bb358ed1b277ac7d27597c024f9cb8ef4b0d3
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-11 00:46:40 +0530

    Regex Regular expression

--- src/Regex.java
+++ src/Regex.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 //The Java Regex or Regular Expression is an API to define a pattern for searching or manipulating strings.
 //Matcher class
 //It implements the MatchResult interface.It is a regex engine which is used to perform match operations on a character sequence.
@@ -15,7 +17,7 @@ public class Regex{
     boolean b3=Pattern.matches(".s","as");
         System.out.println(b+" "+b2+" "+b3);
     }
-}*/
+}
 import java.util.regex.*;
 public class Regex{
     public static void main(String[] args) {
@@ -24,5 +26,13 @@ public class Regex{
         System.out.println(Pattern.matches(".s","mst"));//false (has more than 2 char)
         System.out.println(Pattern.matches(".s","amms"));//false (has more than 2 char)
         System.out.println(Pattern.matches("..s","mas"));//true (3rd char is s)
+    }
+}*/
+//Regular Expression Character classes Example
+public class Regex{
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches("[amn]","abcd"));//false (not a or m or n)
+        System.out.println(Pattern.matches("[amn]","a"));//true (among a or m or n)
+        System.out.println(Pattern.matches("[amn]","ammna"));//false (m and a comes more than once)
     }
 }
