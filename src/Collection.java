77c9313deec30229cc1117ac20e0d53a8bafaca7
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-13 12:01:16 +0530

    Collection Framework

--- src/Collection.java
+++ src/Collection.java
@@ -18,7 +18,7 @@ public class Collection {
             System.out.println(itr.next());
         }
     }
-}*/
+}
 //LinkedList
 //LinkedList implements the Collection interface. It uses a doubly linked list internally to store the elements.
 // It can store the duplicate elements.
@@ -31,6 +31,38 @@ public class Collection{
         l1.add("Ravi");
         l1.add("akansha");
         Iterator<String> itr=l1.iterator();
+        while(itr.hasNext())
+            System.out.println(itr.next());
+    }
+}
+//vector
+import java.util.*;
+public class Collection{
+    public static void main(String[] args) {
+        Vector<String>v=new Vector<String>();
+        v.add("anshika");
+        v.add("amit");
+        v.add("Hritik");
+        v.add("Priyanshi");
+        v.add("akansha");
+        Iterator<String>itr=v.iterator();
+        while(itr.hasNext())
+            System.out.println(itr.next());
+
+
+    }
+}*/
+//stack
+import java.util.*;
+public class Collection{
+    public static void main(String[] args) {
+        Stack<String>s1=new Stack<String>();
+        s1.push("anshika");
+        s1.push("amit");
+        s1.push("Hritik");
+        s1.push("Priyanshi");
+        s1.push("akansha");
+        Iterator<String>itr=s1.iterator();
         while(itr.hasNext())
             System.out.println(itr.next());
     }
