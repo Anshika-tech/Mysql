b85fc8d9c50783a9816533ea463028d279021125
Anshika-tech <srivastavanshika10@gmail.com>
2021-12-13 23:35:30 +0530

    Generic concept

--- src/Generics.java
+++ src/Generics.java
@@ -1,5 +1,5 @@
 //Example of generic concept
-import java.util.*;
+/*import java.util.*;
 public class Generics {
     public static void main(String[] args) {
         ArrayList<String>l1=new ArrayList<String>();
@@ -14,5 +14,25 @@ public class Generics {
         while(itr.hasNext())
             System.out.println(itr.next());
 
+    }
+}*/
+//Generic class
+import java.util.*;
+class MyGen<T>{
+    T obj;
+    void add(T obj){
+        this.obj=obj;
+    }
+    T get(){
+        return obj;
+    }
+}
+public class Generics{
+    public static void main(String[] args) {
+        MyGen<Integer>m=new MyGen<Integer>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        m.add(sc.nextInt());
+        System.out.println(m.get());
     }
 }
